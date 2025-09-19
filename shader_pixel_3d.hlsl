7214cebde2529334719815217f99a1228f0d271a
ChihiroJP <chihaojapan@gmail.com>
2025-09-20 01:15:58 +0900

    encoding

---
+++ shader_pixel_3d.hlsl
@@ -0,0 +1,22 @@
+/*==============================================================================
+
+   3D Pixel Shader [shader_pixel_3d.hlsl]
+                                                         Author : CheeHow
+                                                         Date   : 2025/09/09
+--------------------------------------------------------------------------------
+
+==============================================================================*/
+struct PS_IN
+{
+    float4 posH : SV_POSITION;
+    float4 color : COLOR0;
+    float2 uv : TEXCOORD0;
+};
+
+Texture2D tex;
+SamplerState samp;
+
+float4 main(PS_IN pi) : SV_TARGET
+{
+    return tex.Sample(samp, pi.uv) * pi.color;
+}
